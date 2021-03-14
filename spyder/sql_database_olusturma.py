4f11698b276a4c9aa6462e9478f844a11021de94
Eray ÇINAR <69402551+cinareray@users.noreply.github.com>
2021-03-14 14:32:27 +0300

    Delete spyder directory

--- spyder/sql_database_olusturma.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector as sql
-
-db = sql.connect(host = 'localhost',
-                 user = 'root', 
-                 password = 'mysql1234')
-
-myc = db.cursor()
-myc.execute('CREATE DATABASE deneme')
--- spyder/sql_database_tablo_olusturma.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector as sql
-
-db = sql.connect(host = 'localhost',
-                 user = 'root',
-                 password = 'mysql1234',
-                 database = 'shopapp')
-
-mycursor = db.cursor()
-mycursor.execute('CREATE TABLE python_data (id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, last_name  VARCHAR(45), first_name VARCHAR(45))')
